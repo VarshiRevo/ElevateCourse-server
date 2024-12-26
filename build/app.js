e99b8e01f9771d8ad6ef665a168502baff821dc6
Varshiga P S <145317472+VarshiRevo@users.noreply.github.com>
2024-12-26 13:41:55 +0530

    Update app.js

--- build/app.js
+++ build/app.js
@@ -23,7 +23,7 @@ exports.app.use(express_1.default.json({ limit: "50mb" }));
 exports.app.use((0, cookie_parser_1.default)());
 // cors => cross origin resource sharing
 exports.app.use((0, cors_1.default)({
-    ORIGIN = ['https://elevatecourse-client.vercel.app', 'https://elevatecourse-client-5ieqp3iq5-varshigas-projects.vercel.app']
+    ORIGIN = ['https://elevatecourse-client.vercel.app', 'https://elevatecourse-client-5ieqp3iq5-varshigas-projects.vercel.app'],
     credentials: true,
 }));
 // api requests limit
